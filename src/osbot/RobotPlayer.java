ca6c0c2b7faf1b6f7cec88a19ef657558242f1b8
Otmas <Oatmas64134@gmail.com>
2017-02-11 13:14:06 -0500

    Update some stuff

--- src/osbot/RobotPlayer.java
+++ src/osbot/RobotPlayer.java
@@ -3,14 +3,14 @@ import battlecode.common.*;
 
 public class RobotPlayer {
 	static RobotController rc;
-
+	static int noLumber = 0;
+	static int noSoldier = 0;
 	//static Direction dir = randomDirection();
 
 	public static void run(RobotController rc) throws GameActionException {
 		RobotPlayer.rc = rc;
 		switch (rc.getType()) {
 		case ARCHON:
-
 			runArchon();
 			break;
 		case GARDENER:
@@ -53,11 +53,21 @@ public class RobotPlayer {
 
 			Direction dir = randomDirection();
 			try {
-
-				if (rc.canBuildRobot(RobotType.LUMBERJACK, dir)) {
+				TreeInfo[] treeLoc = rc.senseNearbyTrees(rc.getType().sensorRadius, rc.getTeam().NEUTRAL);
+				TreeInfo[] myTreeLoc = rc.senseNearbyTrees(rc.getType().sensorRadius, rc.getTeam());
+				RobotInfo[] robotLoc = rc.senseNearbyRobots(rc.getType().sensorRadius, rc.getTeam().opponent());
+				if (rc.canBuildRobot(RobotType.LUMBERJACK, dir) && treeLoc.length > 0 /* noLumber <= 10*/ ) {
 					rc.buildRobot(RobotType.LUMBERJACK, dir);
-				} else if (rc.canPlantTree(dir)) {
+					//noLumber = noLumber++;
+				} 
+				if (rc.canBuildRobot(RobotType.SOLDIER, dir) /*&& robotLoc.length > 0 && noSoldier <= 10 */) {
+					rc.buildRobot(RobotType.SOLDIER, dir);
+				} 
+				if (rc.canPlantTree(dir)) {
 					rc.plantTree(dir);
+					if (rc.canShake()) {
+						rc.shake(myTreeLoc[0].location);
+					}
 				} 
 				Clock.yield();
 			} catch(Exception e) {
@@ -70,21 +80,23 @@ public class RobotPlayer {
 	public static void runSoldier() throws GameActionException {
 		while(true) {
 			try {
-				RobotInfo[] robots = rc.senseNearbyRobots(30, rc.getTeam().opponent());
+				RobotInfo[] robots = rc.senseNearbyRobots(rc.getType().sensorRadius, rc.getTeam().opponent());
 
 				if (robots.length > 0) {
 					MapLocation myLoc = rc.getLocation();
 					MapLocation robotLoc = robots[0].getLocation();
 					Direction fireDir = myLoc.directionTo(robotLoc);
+					rc.setIndicatorDot(robotLoc, 0, 255, 0);
 					if (rc.canFirePentadShot()) {
 						rc.firePentadShot(fireDir);
 					} else if (rc.canFireTriadShot()){
 						rc.fireTriadShot(fireDir);
 					} else if (rc.canFireSingleShot()) {
 						rc.fireSingleShot(fireDir);
-					} else {
-						Clock.yield();
-					}
+					} 
+					move(randomDirection());
+					Clock.yield();
+
 				}
 				Clock.yield();
 			} catch(Exception e) {
@@ -101,18 +113,37 @@ public class RobotPlayer {
 		while (true) {
 
 			TreeInfo[] treeLocations = rc.senseNearbyTrees(rc.getType().sensorRadius, rc.getTeam().NEUTRAL);
+			RobotInfo[] robotLocations = rc.senseNearbyRobots(rc.getType().sensorRadius, rc.getTeam().opponent());
 			try {
 				if (treeLocations.length > 0) {
 
 					MapLocation myLocation = rc.getLocation();
 					MapLocation treeLocation = treeLocations[0].getLocation();
 					Direction toTree = myLocation.directionTo(treeLocation);
-					move(toTree);
+					if (rc.canMove(toTree)){
+						move(toTree);
+					}	
 					if (rc.canChop(treeLocations[0].ID)) {
 						rc.chop(treeLocations[0].ID);
 					}
-
-				} else {
+				}/* else if (rc.canBuildRobot(RobotType.SOLDIER, dir)) {
+					rc.buildRobot(RobotType.SOLDIER, dir);
+				} */if (/*treeLocations.length == 0 &&*/ robotLocations.length > 0) {
+					MapLocation myLocation = rc.getLocation();
+					MapLocation robotLoc = robotLocations[0].getLocation();
+					Direction toBot = myLocation.directionTo(robotLoc);
+					
+					if (rc.canMove(toBot)) {
+						rc.setIndicatorLine(myLocation, robotLoc, 255, 0, 0);
+						move(toBot);
+						if (rc.canStrike()) {
+							System.out.println("I strike!");
+							rc.strike();
+						}
+					}
+					//rc.disintegrate();
+				}
+				else {
 					move(dir);
 				}
 				Clock.yield();
